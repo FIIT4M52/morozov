6072cf0fba9a8e24332dc20f68c593c3294bebf9
FIIT4M52 <rff4-8-13@yandex.ru>
2017-12-17 21:21:21 +0300

    chek_noti

---
+++ chek_reg.py
@@ -0,0 +1,29 @@
+import random as rand
+import string
+from xlutils.copy import copy
+import xlwt, xlrd
+
+class chek_email():
+    rand.seed()
+
+    def __init__(self):
+        #self.sum = int(rand.uniform(10000000,99999999))
+        self.token = ''.join(rand.choice(string.ascii_lowercase + string.digits) for x in range(16))
+    #send to user and chek_control
+
+    def send_key(self):
+        print(self.token)
+
+    def chek_control(self, key, email):
+        global control_sum
+        if key == self.sum:
+            read = xlrd.open_workbook("path")
+            sheetr = read.sheet_by_index(0)
+            wb_dst = copy(sheetr)
+            ws_dst = wb_dst.get_sheet(0)
+            for i in range(0,sheetr.ncols):
+                if sheetr.cell(i,2).value == email:
+                    ws_dst.write(i, 6, 'true')
+                else:
+                    ws_dst.write(i, 6, 'false')
+        wb_dst.save("path")
---
+++ notification.py
@@ -0,0 +1,24 @@
+import xlutils.filter as xls
+import xlwt, xlrd
+
+
+class toast():
+
+    def __init__(self):
+        self.weight = 0
+        self.area = "" #лучше тут конечно же что бы выбиралось из списка
+
+    def send_to_user(self):
+        pass
+
+    def get_request(self, start, end):
+        i = 0
+        mas=[]
+        read = xlrd.open_workbook("path")
+        sheetr = read.sheet_by_index(0)
+        for i in range(start, end):
+            if i <=10:
+                if self.weight < sheetr.cell(i,3).value or self.area == sheetr.cell(i,4).value:
+                    mas.append(sheetr.sheet_by_index(0).cell(i,0).value)
+                    i+=1
+        return mas
