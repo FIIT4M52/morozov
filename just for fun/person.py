24f3cb72789d0b39c5d8b40a778deff842b14aa8
FIIT4M52 <34235939+FIIT4M52@users.noreply.github.com>
2018-04-02 20:00:33 +0300

    1
    (copyright)Pyatugin

---
+++ just for fun/person.py
@@ -0,0 +1,10 @@
+class Person:
+
+    def __init__(self, id, name, phone, area, placedorder, giveOrder, rate):
+        self.id = id
+        self.name = name
+        self.phone = phone
+        self.area = area
+        self.placedOrder = placedorder
+        self.giveOrder = giveOrder
+        self.rate = rate
